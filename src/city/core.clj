1c07c127739bc39fa44004f110d67e186572eac4
Eric Coleman <eric@epiccoleman.com>
2017-02-15 09:04:52 -0500

    Generate buildings with (some) randomness

--- src/city/core.clj
+++ src/city/core.clj
@@ -5,7 +5,18 @@
 (defn setup []
   ; setup function returns initial state. It contains
   ; circle color and position.
-  [])
+  (def buildings-cnt 5)
+  
+  (vec (loop [ cnt buildings-cnt 
+          buildings '([ 20 200 50 100])]
+    (if (= 0 cnt) 
+      buildings 
+      (recur (dec cnt) 
+             (conj buildings 
+                   [(* cnt 70) 
+                    200 
+                    50 
+                    (+ (rand 100) 100)]))))))
 
 (defn draw [state]
   ; Clear the sketch by filling it with black color.
@@ -15,7 +26,7 @@
   (q/fill 255 255 255)
 
   ; Draw a rectangle
-  (doseq [rect [[250 250 70 80] [300 250 70 60]]] 
+  (doseq [rect state] 
     (apply q/rect rect))
 )
 
